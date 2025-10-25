88d6d62efbc7a7382e6c5c85e7d56b755adcec32
regret546 <iregret546@gmail.com>
2025-10-26 06:32:59 +0800

    add assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,25 @@ import Button from "./UI/Button";
 const Navbar = () => {
   return (
     <nav className="flex w-full justify-between items-center py-[1.5rem] px-[3rem] bg-dark text-white">
-      <div>Logo</div>
+      <img
+        className="w-[80px] h-[80px]"
+        src="/src/assets/logo-white.png"
+        alt="logo"
+      />
       <div className="flex gap-8 items-center">
-        {" "}
         <ul className="flex gap-[2rem] text-[1.2rem]">
-          <li className="cursor-pointer">Home</li>
-          <li className="cursor-pointer">Episodes</li>
-          <li className="cursor-pointer">Reviews</li>
+          {["Home", "Episodes", "Reviews"].map((item) => (
+            <li
+              key={item}
+              className="relative cursor-pointer font-semibold text-white 
+                         after:content-[''] after:absolute after:left-0 after:bottom-[-4px]
+                         after:w-0 after:h-[2px] after:bg-[#FCAB1C]
+                         after:transition-all after:duration-300
+                         hover:text-[#FCAB1C] hover:after:w-full"
+            >
+              {item}
+            </li>
+          ))}
         </ul>
         <Button text="Subscribe" />
       </div>
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import Button from "../components/UI/Button";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="flex flex-col-reverse md:flex-row bg-primary p-[2rem] pb-0 relative">
+        {/* LEFT CONTENT */}
+        <div className="w-full md:w-1/2 flex items-center pb-[2rem]">
+          <div className="w-full md:w-[85%]">
+            <h2 className="text-[2rem]">Welcome to</h2>
+            <h1 className="text-[3.5rem] text-white">Group Chat IRL Podcast</h1>
+            <p className="text-[1.25rem]">
+              Weekly Bardagulan with Vivax and Friends. Lorem Ipsum is simply
+              dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to
+              make a type specimen book. It has survived not only five
+              centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s
+              with the release of Letraset sheets containing.
+            </p>
+            <Button
+              text="All Episodes"
+              className="bg-accent hover:bg-accent/80 mt-[1rem] px-[3rem] transition-none"
+            />
+          </div>
+        </div>
+
+        {/* RIGHT IMAGE */}
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+          <img
+            className="w-full h-auto max-w-[600px] md:max-w-[900px] object-contain"
+            src="/src/assets/group-photo.png"
+            alt="Group photo"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
